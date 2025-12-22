58c678556d1351c2281e16e95d358af11314ef48
aposte7 <olman4jesus@gmail.com>
2025-12-22 11:14:37 +0300

    refactor: encapsulate state variables and update access modifiers

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -2,33 +2,20 @@
 pragma solidity ^0.8.0;
 
 contract CrowdfundingCampaign {
-   string campaignName;
-   string campaignDescription;
-   uint256 fundGoal;
-   uint totalFund;
-   string owner;
+   string private campaignName;
+   string private campaignDescription;
+   uint256 private fundGoal;
+   uint private totalFund;
+   address private owner;
 
-   uint256 deadline;
-   uint256 minimumFund;
-   bool isActive;
+   uint256 private deadline;
+   uint256 private minimumFund;
+   bool private isActive;
+
+   constructor(){
+        owner = msg.sender;
+   }
     
-
-    function getName() public view returns(string memory){
-        return campaignName;
-    } 
-
-    function getDescription() public view returns(string memory){
-        return campaignDescription;
-    } 
-
-    function getFundGoal() public view returns(uint){
-        return fundGoal;
-    } 
-
-    function getStatus() public view returns(bool){
-        return isActive;
-    }
-
     function createCampaign(string calldata _campaignName, string calldata _campaignDescription, uint _fundGoal, uint _deadline, uint256 _minimumFund) public {
         require(_deadline >= block.timestamp + 1 hours, "deadline should be at least 1 hour from now");
         require(bytes(_campaignName).length >= 3, "campaign name should be at least 3 characters");
@@ -42,6 +29,58 @@ contract CrowdfundingCampaign {
         minimumFund = _minimumFund;
         isActive = true;
     }
+
+
+    function getName() external view returns (string memory) {
+        return campaignName;
+    }
+
+    function getDescription() external view returns (string memory) {
+        return campaignDescription;
+    }
+
+    function getFundingGoal() external view returns (uint256) {
+        return fundGoal;
+    }
+
+    function getCreator() external view returns (address) {
+        return owner;
+    }
+
+    function getTotalFundsRaised() external view returns (uint256) {
+        return totalFund;
+    }
+
+    function getDeadline() external view returns (uint256) {
+        return deadline;
+    }
+
+    function getMinContribution() external view returns (uint256) {
+        return minimumFund;
+    }
+
+    function getIsActive() external view returns (bool) {
+        return isActive;
+    }
+
+    function getRemainingFunding() external view returns (uint256) {
+        if (totalFund >= fundGoal) {
+            return 0;
+        }
+        return fundGoal - totalFund;
+    }
+
+    function getDaysUntilDeadline() external view returns (uint256) {
+        if (block.timestamp >= deadline) {
+            return 0;
+        }
+        return (deadline - block.timestamp) / 86400; 
+    }
+
+    function isAcceptingDonations() public view returns (bool) {
+        return isActive && (block.timestamp < deadline) && (totalFund < fundGoal);
+    }
+
 
     function setStatus() public {
         uint256 currentTime = block.timestamp;
